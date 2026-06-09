fee0a559e591791904c4814e481f9a5f9246a667
Emmanuel Inambao <denuelinambao@gmail.com>
2026-06-09 12:54:12 +0200

    Fit chat workspace to viewport

--- frontend/src/app/chat/page.tsx
+++ frontend/src/app/chat/page.tsx
@@ -2,7 +2,7 @@ import { ChatShell } from '../../components/chat-shell';
 
 export default function ChatPage() {
   return (
-    <main className='hero'>
+    <main className='hero chat-page'>
       <div className='shell'>
         <ChatShell />
       </div>
