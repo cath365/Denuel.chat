0140c67d67063a956c1c74ef627d4eff3ecfbb56
Emmanuel Inambao <denuelinambao@gmail.com>
2026-06-09 18:19:49 +0200

    Stop preloading header logo

--- frontend/src/components/brand-lockup.tsx
+++ frontend/src/components/brand-lockup.tsx
@@ -25,7 +25,6 @@ export function BrandLockup({
         alt={`${env.appName} logo`}
         className='brand-lockup-image'
         height={imageSize}
-        priority
         src='/branding/logo-primary.jpg'
         width={imageSize}
       />
